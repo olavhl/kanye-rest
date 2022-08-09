5788dd8588168e2d168f86fe533f48ee7c3442fa
Olav <ohalar@gmail.com>
2022-08-09 12:02:13 +0200

    Created dataclass for ApiResult

---
+++ app/src/main/java/com/olav/kanye_rest/model/KanyeQuote.kt
@@ -0,0 +1,5 @@
+package com.olav.kanye_rest.model
+
+data class KanyeQuote(
+    val quote: String
+)
--- app/src/main/java/com/olav/kanye_rest/retrofit/KanyeApi.kt
+++ app/src/main/java/com/olav/kanye_rest/retrofit/KanyeApi.kt
@@ -1,9 +1,10 @@
 package com.olav.kanye_rest.retrofit
 
+import com.olav.kanye_rest.model.KanyeQuote
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface KanyeApi {
     @GET("/")
-    fun getQuote(): Call<String>
+    fun getQuote(): Call<KanyeQuote>
 }
